bf942ccfa4111c76b7627853ecdba8d45235c508
Adebowale Adewale <adewalewale55@gmail.com>
2023-11-01 12:01:54 -0700

    Print the alphabet in reverse order alternating upper- and lower-case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Prints the alphabet in reverse order, alternating lowercase and uppercase"""
 i = 0
-for char in range(ord('z'), ord('a') -1, -1):
+for char in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(char - i)), end="")
     i = 32 if i == 0 else 0
