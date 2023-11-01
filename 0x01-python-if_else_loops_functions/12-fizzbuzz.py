f997998342b604cc8ee23b7cbd7d4ca704e720b3
Adebowale Adewale <adewalewale55@gmail.com>
2023-11-01 11:22:33 -0700

    Fizzbuzz program

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,12 +5,12 @@ def fizzbuzz():
         and for multiples of five print Buzz
         For numbers which are multiples of both three and five print FizzBuzz
         """
-        for num in range(1, 101):
-            if num % 3 == 0 and num % 5 == 0:
-                print("FizzBuzz", end=" ")
-            elif num % 3 == 0:
-                print("Fizz", end=" ")
-            elif num % 5 == 0:
-                print("Buzz", end=" ")
-            else:
-                print(num, end=" ")
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(num, end=" ")
