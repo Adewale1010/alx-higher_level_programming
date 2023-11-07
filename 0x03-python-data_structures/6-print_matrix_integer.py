767ddd4db9ba25169cb140b0a1c0196949849d53
Adebowale Adewale <adewalewale55@gmail.com>
2023-11-07 09:27:40 -0800

    Print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers."""
-    for i in range(len(matix)):
+    for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end="")
             if j != (len(matrix[i]) - 1):
