e31c2dc2838a576e8cde66ae7ef913fe08decb66
Adebowale Adewale <adewalewale55@gmail.com>
2023-11-08 17:28:09 -0800

    Replace all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     """Replaces all occurrences of an element by another in a new list"""
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if new_list[i] = search:
+        if new_list[i] == search:
             new_list[i] = replace
     return (new_list)
